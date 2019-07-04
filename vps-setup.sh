860d4b399d97b096c2097d072088d3d627851060
Karl <x>
2019-07-04 19:01:21 +0200

    adding new user works.

--- vps-setup.sh
+++ vps-setup.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 # VPS setup script over ssh.
 # Arguments:
-#   1: user@IP
+#   1: root@IP
+#   2: Username to create on the remote.
 
-ssh "$1" <<'ENDSSH'
-df
-ENDSSH
+# Add user
+echo "Enter the old VPS root password and then new password for $2"
+ssh -t "$1" 'useradd --create-home '"$2"'; passwd '"$2"
+
+#ssh -t "$1" <<'ENDSSH'
+#	apt update
+#	apt upgrade
+#	apt autoremove
+	
+#	echo "Adding a new user"
+#	adduser "$2"
+#	usermod -aG sudo "$2"
+#ENDSSH
+
+# Changing root password
+#ssh -t "$1" passwd
+
+# Generating SSH keys and pushing it to new user.
+
+# Connecting using new key, setup SSH on the remote and add local entry.
