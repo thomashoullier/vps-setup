4babc18bae48bd940e8d6ba087205c4fde61c72b
Karl <x>
2019-07-06 09:24:42 +0200

    sshd_config port input and local ssh entry addition work

--- vps-setup.sh
+++ vps-setup.sh
@@ -4,24 +4,29 @@
 #   1: IP
 #   2: Username to create on the remote.
 #   3: File in which to put the SSH key on local machine.
+#   4: Port to use for ssh connections with remote.
 
 # Add user and change root password.
 echo "Enter:
   * Current root password
   * New root password
   * New user password"
-ssh -t root@"$1" 'passwd; useradd '"$2"';'\
-'usermod -aG sudo '"$2"'; passwd '"$2"'; apt update; apt upgrade;'\
+ssh -t root@"$1" 'passwd; adduser '"$2"';'\
+'usermod -aG sudo '"$2"'; apt update; apt upgrade;'\
 'apt autoremove'
 
-# Generating SSH keys, pushing it to new user and adding it locally.
+# Generating SSH keys, and pushing it to new user.
 ssh-keygen -f "$3"
 ssh-copy-id -i "$3".pub "$2"@"$1"
+
+# Loading the local SSH configuration file.
+sshconfig=$(cat sshd_config)
+#     Input correct Port
+sshconfig_remote=$(echo "$sshconfig" | sed '/Port 0/c\Port '"$4"'')
+
 echo "Adding key to local ~/.ssh/config"
 echo 'Host '"$1"'
   User '"$2"'
   IdentityFile '"$3"'
+  Port '"$4"'
 ' | sudo tee -a ~/.ssh/config
-
-# Connecting using new key, setup SSH on the remote and add local entry.
-
